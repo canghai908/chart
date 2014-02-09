70ee811c430a429cabb529f91f86a89beb9277a8
canghai908 <lovecanghai@gmail.com>
2014-02-09 17:01:19 +0800

    add templates
    add templates

--- cacti/views.py
+++ cacti/views.py
@@ -1,8 +1,18 @@
 # Create your views here.
 from django.shortcuts import render_to_response
-from django.http import HttpResponse
-from cacti.models import health
+from django.http import HttpResponse,HttpResponseRedirect
+from cacti.models import health,User
+from django import forms
 
 def show(request):
     hosts = health.objects.all()
     return render_to_response('show.html',{'hosts':hosts})
+class UserForm(forms.Form):
+	usrename = forms.CharField()
+	password = forms.CharField(widget = forms.PasswordInput)
+def regist(request):
+	if request.method =="POST":
+		uf = UserForm(request.POST)
+		if uf.is_valid():
+			usrename = uf.clean_data['username']
+			
