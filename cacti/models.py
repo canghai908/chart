0e2dbabd06e88391f7f3f535e7f11e83ce4d75e0
canghai908 <lovecanghai@gmail.com>
2014-02-09 11:39:12 +0800

    modles add ping
    modles add ping

--- cacti/models.py
+++ cacti/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class health(models.Model):
     host = models.CharField(max_length=30)
+    ping = models.IntegerField(max_length=30)
     def __unicode__(self):
     	return self.host
 
