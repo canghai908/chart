c433e20ffb4ab075d0a1559425479ae5eef65b71
canghai908 <lovecanghai@gmail.com>
2014-02-09 21:30:28 +0800

    fixbug1
    fixbug1

--- cacti/views.py
+++ cacti/views.py
@@ -14,5 +14,35 @@ def regist(request):
 	if request.method =="POST":
 		uf = UserForm(request.POST)
 		if uf.is_valid():
-			usrename = uf.clean_data['username']
-			
+			username = uf.cleaned_data['username']
+			password = uf.cleaned_data['password']
+			User.objects.create(username = username,password = password)
+			return HttpResponseRedirect('/login/')
+	else:
+		uf = UserForm()
+	return render_to_response('regist.html',{'uf':uf})
+def login(request):
+	if request.method == 'POST':
+		uf = UserForm(request.POST)
+		if uf.is_valid();
+			username = uf.cleaned_data['username']
+			password = uf.cleaned_data['password']
+			user = User.objects.filter(username__exact = username, password_exact = password)
+			if user:
+				request.session['username'] = username
+				return HttpResponseRedirect('/index/')
+			else:
+				return HttpResponseRedirect('/login/')
+	else:
+		uf = UserForm()
+	return render_to_response('login.html',{'uf':uf})
+def index(request):
+	username = request.session.get('username','anybody')
+	return render_to_response('index.html',{'username':username})
+def logout(request):
+	session = request.session.get('username',False)
+	if session:
+		del request.session['username']
+		return render_to_response('login.html',{'username':username})
+	else:
+		return HttpResponse('please login!')
