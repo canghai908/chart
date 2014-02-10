a0aa6db7903086ff9571d34edef8e265c6ff9f90
canghai908 <lovecanghai@gmail.com>
2014-02-10 17:43:36 +0800

    bug
    bug

--- cacti/views.py
+++ cacti/views.py
@@ -42,7 +42,7 @@ def login(request):
 def index(request):
 	hosts = health.objects.all()
 	username = request.session.get('username','anybody')
-	return render_to_response('index.html',{'username':username},{'hosts':hosts})
+	return render_to_response('index.html',{'username':username,'hosts':hosts})
 def logout(request):
 	session = request.session.get('username',False)
 	if session:
