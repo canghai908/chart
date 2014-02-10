db7d7d20367ec7d1eb99ebb711771e2ffc9fe940
canghai908 <lovecanghai@gmail.com>
2014-02-10 15:25:03 +0800

    addform
    addform

--- chart/settings.py
+++ chart/settings.py
@@ -133,7 +133,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'cacti',
-    'bootstrapform',
+    #'bootstrapform',
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
