9a558c0453caf8ca19d94f91577a43261a920e1a
canghai908 <lovecanghai@gmail.com>
2014-02-11 16:55:40 +0800

    add

--- cacti/models.py
+++ cacti/models.py
@@ -5,8 +5,8 @@ class health(models.Model):
     host = models.CharField(max_length=30)
     ping = models.IntegerField(max_length=30)
     date = models.IntegerField(max_length=30)
-    traffic_in = models.FloatField(max_digits=19,decimal_places=10)
-    traffic_out = models.FloatField(max_digits=19,decimal_places=10)
+    traffic_in = models.DecimalField(max_digits=19,decimal_places=10)
+    traffic_out = models.DecimalField(max_digits=19,decimal_places=10)
     def __unicode__(self):
     	return self.host
 class User(models.Model):
