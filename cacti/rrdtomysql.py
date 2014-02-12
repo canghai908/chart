3f590a3f59e8a80d8ce5d3f2f990fc877419e808
canghai908 <lovecanghai@gmail.com>
2014-02-12 10:15:44 +0800

    tet
    test

--- cacti/rrdtomysql.py
+++ cacti/rrdtomysql.py
@@ -11,11 +11,14 @@ endtime = data[0][1]
 step = data[0][2]
 time = (endtime - firsttime)/step
 
-conn = MySQLdb.connect(host="localhost",user="app",passwd="tften8sfpkz45hpw",db="app",charset="utf8")
+conn = MySQLdb.connect(host="localhost",user="chart",passwd="tobaby",db="chart",charset="utf8")
 db = conn.cursor()
-sql = "insert into show_rss values(NULL,WEB,35,11111111,%s,%s)"
-for x in counts:
-	 db.execute(sql，
+sql = "insert into cacti_health values(NULL,WEB,35,11111111,%s,%s)"
+for x in time:
+	db.execute(sql,data[2][x][0],data[2][x][1])
+conn.commit()
+conn.close()
+"""
 count = 0
 while count <5761:
 	time = firsttime+15*count
@@ -35,3 +38,4 @@ for x in counts:
     db.execute(sql,[feed_.entries[x].title,BeautifulSoup(feed_.entries[x].content[0].value).findAll('img')[0]['src'],feed_.entries[x].link,BeautifulSoup(feed_.entries[x].content[0].value).findAll('p')[0],parser.parse(feed_.entries[x].published).strftime("%Y-%m-%d %I:%M:%S"),random.randint(1000, 9999),random.randint(100, 300)])
 conn.commit()
 conn.close()
+"""
